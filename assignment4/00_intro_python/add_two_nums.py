b6b528afe2a4b2c354d380ec6dde264cf50f8bfb
rehanmirza76 <abdulrehan0317@gmail.com>
2025-03-30 22:43:54 +0500

    update

---
+++ assignment4/00_intro_python/add_two_nums.py
@@ -0,0 +1,8 @@
+def main():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    total = num1 + num2
+    print(f"The sum of {num1} and {num2} is {total}")
+
+if __name__ == '__main__':
+    main()
